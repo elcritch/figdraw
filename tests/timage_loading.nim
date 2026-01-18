e7ce3fd981187396ecac6c54e8b8863dcfe50922
Jaremy Creechley <creechley@gmail.com>
2026-01-17 23:14:15 -0700

    fix test

--- tests/timage_loading.nim
+++ tests/timage_loading.nim
@@ -1,31 +1,27 @@
 import std/[os, unittest]
 
-import pkg/pixie
+import pkg/pixie as pixie except readImage
 
 import figdraw/commons
-import figdraw/opengl/glcontext as glctx
 
 suite "image loading":
   test "load png via figDataDir fallback":
     setFigDataDir(getCurrentDir() / "data")
-    var ctx: glctx.Context
-    let flippy = ctx.loadImage("arrow.png")
+    let flippy = readImage("arrow.png")
     require flippy.mipmaps.len > 0
 
-    let expected = readImage(figDataDir() / "arrow.png")
+    let expected = pixie.readImage(figDataDir() / "arrow.png")
     check flippy.mipmaps[0].width == expected.width
     check flippy.mipmaps[0].height == expected.height
 
   test "load flippy via figDataDir fallback":
     setFigDataDir(getCurrentDir() / "data")
-    var ctx: glctx.Context
-    let flippy = ctx.loadImage("arrow.flippy")
+    let flippy = readImage("arrow.flippy")
     require flippy.mipmaps.len > 0
 
-    let expected = readImage(figDataDir() / "arrow.png")
+    let expected = pixie.readImage(figDataDir() / "arrow.png")
     check flippy.mipmaps[0].width == expected.width
     check flippy.mipmaps[0].height == expected.height
 
   test "missing image returns empty":
-    var ctx: glctx.Context
-    check ctx.loadImage("does-not-exist.png").mipmaps.len == 0
+    check readImage("does-not-exist.png").mipmaps.len == 0
