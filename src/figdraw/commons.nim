ec0f8fe869c220f79c4749c41a5d5d914edb9363
Jaremy Creechley <creechley@gmail.com>
2026-02-01 08:22:29 -0700

    appframe cleanup

--- src/figdraw/commons.nim
+++ src/figdraw/commons.nim
@@ -1,7 +1,6 @@
 import common/shared
 import common/uimaths
 import common/rchannels
-import common/transfer
 import common/fontutils
 import common/imgutils
 
@@ -9,6 +8,5 @@ const UseMetalBackend* {.booldefine: "figdraw.metal".} =
   defined(macosx) and defined(feature.figdraw.metal)
 
 export shared, uimaths, rchannels
-export transfer
 export fontutils
 export imgutils
