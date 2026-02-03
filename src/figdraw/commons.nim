f1e156917baecc5ee1f6381af1d73e5f648fcd8f
Jaremy Creechley <creechley@gmail.com>
2026-02-03 10:53:24 -0700

    fix UseMetalBackend

--- src/figdraw/commons.nim
+++ src/figdraw/commons.nim
@@ -5,7 +5,7 @@ import common/fontutils
 import common/imgutils
 
 const UseMetalBackend* {.booldefine: "figdraw.metal".} =
-  defined(macosx) and defined(feature.figdraw.metal)
+  defined(macosx) or defined(feature.figdraw.metal)
 
 export shared, uimaths, rchannels
 export fontutils
