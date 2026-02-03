e0136d5feb71cc855634b42a78e17ff20f8f4190
Jaremy Creechley <creechley@gmail.com>
2026-02-03 07:02:58 -0700

    add lottie bouncy ball

--- src/figdraw/lottie/loader.nim
+++ src/figdraw/lottie/loader.nim
@@ -1,5 +1,3 @@
-import std/os
-
 import ./types
 import pkg/jsony
 
--- tests/tlottie_bouncy_ball.nim
+++ tests/tlottie_bouncy_ball.nim
@@ -1,98 +1,15 @@
-import std/[unittest]
+import std/[os, unittest]
 
 import figdraw/lottie/loader
 import figdraw/lottie/render
 import figdraw/common/imgutils
 import figdraw/fignodes
-
-const BouncyBallJson = """
-{
-  "nm": "Bouncy Ball",
-  "v": "5.5.2",
-  "ip": 0,
-  "op": 120,
-  "fr": 60,
-  "w": 512,
-  "h": 512,
-  "layers": [
-    {
-      "ddd": 0,
-      "ty": 4,
-      "ind": 0,
-      "st": 0,
-      "ip": 0,
-      "op": 120,
-      "nm": "Layer",
-      "ks": {
-        "a": { "a": 0, "k": [0, 0] },
-        "p": { "a": 0, "k": [0, 0] },
-        "s": { "a": 0, "k": [100, 100] },
-        "r": { "a": 0, "k": 0 },
-        "o": { "a": 0, "k": 100 }
-      },
-      "shapes": [
-        {
-          "ty": "gr",
-          "nm": "Ellipse Group",
-          "it": [
-            {
-              "ty": "el",
-              "nm": "Ellipse",
-              "p": { "a": 0, "k": [204, 169] },
-              "s": { "a": 0, "k": [153, 153] }
-            },
-            {
-              "ty": "fl",
-              "nm": "Fill",
-              "o": { "a": 0, "k": 100 },
-              "c": { "a": 0, "k": [0.710, 0.192, 0.278] },
-              "r": 1
-            },
-            {
-              "ty": "tr",
-              "a": { "a": 0, "k": [204, 169] },
-              "p": {
-                "a": 1,
-                "k": [
-                  { "t": 0, "s": [235, 106], "h": 0,
-                    "o": { "x": [0.333], "y": [0] },
-                    "i": { "x": [1], "y": [1] }
-                  },
-                  { "t": 60, "s": [235, 441], "h": 0,
-                    "o": { "x": [0], "y": [0] },
-                    "i": { "x": [0.667], "y": [1] }
-                  },
-                  { "t": 120, "s": [235, 106] }
-                ]
-              },
-              "s": {
-                "a": 1,
-                "k": [
-                  { "t": 55, "s": [100, 100], "h": 0,
-                    "o": { "x": [0], "y": [0] },
-                    "i": { "x": [1], "y": [1] }
-                  },
-                  { "t": 60, "s": [136, 59], "h": 0,
-                    "o": { "x": [0], "y": [0] },
-                    "i": { "x": [1], "y": [1] }
-                  },
-                  { "t": 65, "s": [100, 100] }
-                ]
-              },
-              "r": { "a": 0, "k": 0 },
-              "o": { "a": 0, "k": 100 }
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
-"""
+import figdraw/commons
 
 suite "lottie bouncy ball":
   test "renders mtsdf ellipse at keyframes":
-    let anim = parseLottie(BouncyBallJson)
+    setFigDataDir(getCurrentDir() / "data")
+    let anim = loadLottieFile(figDataDir() / "lottie" / "bouncy_ball.json")
     var renderer = initLottieMtsdfRenderer(anim)
 
     let renders0 = renderer.renderLottieFrame(0.0'f32)
