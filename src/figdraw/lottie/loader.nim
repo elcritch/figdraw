363bf36b306b78eb0ec3b411c3a5f38ba377866b
Jaremy Creechley <creechley@gmail.com>
2026-02-03 07:30:33 -0700

    lottie moved to github.com/elcritch/lotty

--- src/figdraw/lottie/loader.nim
+++
@@ -1,8 +0,0 @@
-import ./types
-import pkg/jsony
-
-proc parseLottie*(data: string): LottieAnimation =
-  jsony.fromJson(data, LottieAnimation)
-
-proc loadLottieFile*(path: string): LottieAnimation =
-  parseLottie(readFile(path))
