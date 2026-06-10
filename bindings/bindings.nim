c58a76df1c251689f7039d2c851336b3a9111af0
Jaremy Creechley <creechley@gmail.com>
2026-06-09 21:31:11 -0600

    clean up figdraw bindings

--- bindings/bindings.nim
+++ bindings/bindings.nim
@@ -1,14 +1,33 @@
 import genny
 import std/tables
 import std/hashes
+import std/os
 import figdraw/commons
 import figdraw/fignodes as fdn
 import figdraw/common/fonttypes as fnt
 import figdraw/common/fontutils as fut
 
 const ExportSiwinShim* {.booldefine: "figdraw.bindings.siwinshim".} = false
+const GeneratedDir = currentSourcePath().parentDir / "generated"
 
 type
+  BindingFillAxis {.pure.} = enum
+    x
+    y
+    diagTlBr
+    diagBlTr
+
+  BindingFontCase {.pure.} = enum
+    normal
+    upper
+    lower
+    title
+
+  BindingShadowStyle {.pure.} = enum
+    none
+    drop
+    inner
+
   RgbaColor* = object
     r*: uint8
     g*: uint8
@@ -48,9 +67,7 @@ proc newFig(): Fig =
 proc newRgbaColor(r, g, b, a: uint8): RgbaColor =
   RgbaColor(r: r, g: g, b: b, a: a)
 
-proc newCornerRadii(
-    topLeft, topRight, bottomLeft, bottomRight: float32
-): CornerRadii =
+proc newCornerRadii(topLeft, topRight, bottomLeft, bottomRight: float32): CornerRadii =
   CornerRadii(
     topLeft: topLeft,
     topRight: topRight,
@@ -61,13 +78,60 @@ proc newCornerRadii(
 proc newBorderSize(width: float32): BorderSize =
   BorderSize(width: width)
 
+func toFigKind(kind: int8): fdn.FigKind =
+  let raw = kind.int
+  if raw < ord(low(fdn.FigKind)) or raw > ord(high(fdn.FigKind)):
+    fdn.nkFrame
+  else:
+    fdn.FigKind(raw)
+
+func toBindingFillAxis(axis: int8): BindingFillAxis =
+  case axis
+  of 1'i8: BindingFillAxis.y
+  of 2'i8: BindingFillAxis.diagTlBr
+  of 3'i8: BindingFillAxis.diagBlTr
+  else: BindingFillAxis.x
+
+func toFillGradientAxis(axis: BindingFillAxis): FillGradientAxis =
+  case axis
+  of BindingFillAxis.y: fgaY
+  of BindingFillAxis.diagTlBr: fgaDiagTLBR
+  of BindingFillAxis.diagBlTr: fgaDiagBLTR
+  of BindingFillAxis.x: fgaX
+
+func toBindingFontCase(fontCase: int8): BindingFontCase =
+  case fontCase
+  of 1'i8: BindingFontCase.upper
+  of 2'i8: BindingFontCase.lower
+  of 3'i8: BindingFontCase.title
+  else: BindingFontCase.normal
+
+func toFontCase(fontCase: BindingFontCase): fnt.FontCase =
+  case fontCase
+  of BindingFontCase.upper: fnt.FontCase.UpperCase
+  of BindingFontCase.lower: fnt.FontCase.LowerCase
+  of BindingFontCase.title: fnt.FontCase.TitleCase
+  of BindingFontCase.normal: fnt.FontCase.NormalCase
+
+func toBindingShadowStyle(style: int8): BindingShadowStyle =
+  case style
+  of 1'i8: BindingShadowStyle.drop
+  of 2'i8: BindingShadowStyle.inner
+  else: BindingShadowStyle.none
+
+func toShadowStyle(style: BindingShadowStyle): ShadowStyle =
+  case style
+  of BindingShadowStyle.drop: ShadowStyle.DropShadow
+  of BindingShadowStyle.inner: ShadowStyle.InnerShadow
+  of BindingShadowStyle.none: ShadowStyle.NoShadow
+
 proc newRectangleFig(x, y, w, h: float32): Fig =
   Fig(
     inner: fdn.Fig(
       kind: fdn.nkRectangle,
       screenBox: rect(x, y, w, h),
       fill: fill(rgba(255, 255, 255, 255)),
-    ),
+    )
   )
 
 proc newTextFig(x, y, w, h: float32): Fig =
@@ -77,7 +141,7 @@ proc newTextFig(x, y, w, h: float32): Fig =
       screenBox: rect(x, y, w, h),
       fill: fill(rgba(255, 255, 255, 255)),
       textLayout: GlyphArrangement(),
-    ),
+    )
   )
 
 proc newImageFig(x, y, w, h: float32, imageId: int64): Fig =
@@ -87,10 +151,9 @@ proc newImageFig(x, y, w, h: float32, imageId: int64): Fig =
       screenBox: rect(x, y, w, h),
       fill: fill(rgba(255, 255, 255, 255)),
       image: ImageStyle(
-        id: cast[ImageId](Hash(imageId)),
-        fill: fill(rgba(255, 255, 255, 255)),
+        id: cast[ImageId](Hash(imageId)), fill: fill(rgba(255, 255, 255, 255))
       ),
-    ),
+    )
   )
 
 proc newTransformFig(x, y, w, h: float32, tx, ty: float32): Fig =
@@ -99,11 +162,8 @@ proc newTransformFig(x, y, w, h: float32, tx, ty: float32): Fig =
       kind: fdn.nkTransform,
       screenBox: rect(x, y, w, h),
       fill: fill(rgba(255, 255, 255, 255)),
-      transform: TransformStyle(
-        translation: vec2(tx, ty),
-        useMatrix: false,
-      ),
-    ),
+      transform: TransformStyle(translation: vec2(tx, ty), useMatrix: false),
+    )
   )
 
 proc loadTypefaceBinding(name: string): TypefaceRef =
@@ -126,15 +186,7 @@ proc setFigFontLineHeightBinding(font: FigFontRef, lineHeight: float32) =
 proc setFigFontCaseBinding(font: FigFontRef, fontCase: int8) =
   if font.isNil:
     return
-  case fontCase
-  of 1'i8:
-    font.inner.fontCase = fnt.FontCase.UpperCase
-  of 2'i8:
-    font.inner.fontCase = fnt.FontCase.LowerCase
-  of 3'i8:
-    font.inner.fontCase = fnt.FontCase.TitleCase
-  else:
-    font.inner.fontCase = fnt.FontCase.NormalCase
+  font.inner.fontCase = fontCase.toBindingFontCase().toFontCase()
 
 proc typesetTextBinding(
     width, height: float32,
@@ -178,12 +230,17 @@ proc typesetTextBinding(
   except CatchableError:
     nil
 
+proc figWithKind(src: fdn.Fig, kind: fdn.FigKind): fdn.Fig
+
 proc setFigTextLayoutBinding(fig: Fig, layout: GlyphLayoutRef) =
   if fig.isNil or layout.isNil:
     return
-  if fig.inner.kind != fdn.nkText:
-    fig.inner.kind = fdn.nkText
-  fig.inner.textLayout = layout.inner
+  try:
+    if fig.inner.kind != fdn.nkText:
+      fig.inner = figWithKind(fig.inner, fdn.nkText)
+    fig.inner.textLayout = layout.inner
+  except Exception:
+    discard
 
 proc textLayoutWidthBinding(layout: GlyphLayoutRef): float32 =
   if layout.isNil:
@@ -196,10 +253,12 @@ proc textLayoutHeightBinding(layout: GlyphLayoutRef): float32 =
   layout.inner.bounding.h
 
 proc copy(fig: Fig): Fig =
+  if fig.isNil:
+    return nil
   Fig(inner: fig.inner)
 
-proc figWithKind(src: fdn.Fig, kind: int8): fdn.Fig =
-  result = fdn.Fig(kind: fdn.FigKind(kind))
+proc figWithKind(src: fdn.Fig, kind: fdn.FigKind): fdn.Fig =
+  result = fdn.Fig(kind: kind)
   result.zlevel = src.zlevel
   result.parent = src.parent
   result.flags = src.flags
@@ -209,71 +268,85 @@ proc figWithKind(src: fdn.Fig, kind: int8): fdn.Fig =
   result.fill = src.fill
   result.corners = src.corners
 
+proc ensureRectangle(fig: Fig) =
+  if fig.isNil:
+    return
+  if fig.inner.kind != fdn.nkRectangle:
+    fig.inner = figWithKind(fig.inner, fdn.nkRectangle)
+
 proc kind(fig: Fig): int8 =
+  if fig.isNil:
+    return fdn.nkFrame.int8
   fig.inner.kind.int8
 
 proc setKind(fig: Fig, kind: int8) =
-  fig.inner = figWithKind(fig.inner, kind)
+  if fig.isNil:
+    return
+  fig.inner = figWithKind(fig.inner, kind.toFigKind())
 
 proc zLevel(fig: Fig): int8 =
+  if fig.isNil:
+    return 0'i8
   fig.inner.zlevel.int8
 
 proc setZLevel(fig: Fig, zLevel: int8) =
+  if fig.isNil:
+    return
   fig.inner.zlevel = fdn.ZLevel(zLevel)
 
 proc x(fig: Fig): float32 =
+  if fig.isNil:
+    return 0'f32
   fig.inner.screenBox.x
 
 proc y(fig: Fig): float32 =
+  if fig.isNil:
+    return 0'f32
   fig.inner.screenBox.y
 
 proc width(fig: Fig): float32 =
+  if fig.isNil:
+    return 0'f32
   fig.inner.screenBox.w
 
 proc height(fig: Fig): float32 =
+  if fig.isNil:
+    return 0'f32
   fig.inner.screenBox.h
 
 proc setScreenBox(fig: Fig, x, y, w, h: float32) =
+  if fig.isNil:
+    return
   fig.inner.screenBox = rect(x, y, w, h)
 
 proc setFillColor(fig: Fig, r, g, b, a: uint8) =
+  if fig.isNil:
+    return
   fig.inner.fill = fill(rgba(r, g, b, a))
 
 proc setFillColorRgba(fig: Fig, color: RgbaColor) =
+  if fig.isNil:
+    return
   fig.inner.fill = fill(rgba(color.r, color.g, color.b, color.a))
 
-proc parseFillAxis(axis: int8): FillGradientAxis =
-  case axis
-  of 1'i8:
-    fgaY
-  of 2'i8:
-    fgaDiagTLBR
-  of 3'i8:
-    fgaDiagBLTR
-  else:
-    fgaX
-
 proc setFillLinear2Raw(
-    fig: Fig,
-    sr, sg, sb, sa: uint8,
-    er, eg, eb, ea: uint8,
-    axis: int8,
+    fig: Fig, sr, sg, sb, sa: uint8, er, eg, eb, ea: uint8, axis: int8
 ) =
+  if fig.isNil:
+    return
   fig.inner.fill = linear(
     rgba(sr, sg, sb, sa),
     rgba(er, eg, eb, ea),
-    axis = parseFillAxis(axis),
+    axis = axis.toBindingFillAxis().toFillGradientAxis(),
   )
 
-proc setFillLinear2(
-    fig: Fig,
-    startColor, endColor: RgbaColor,
-    axis: int8,
-) =
+proc setFillLinear2(fig: Fig, startColor, endColor: RgbaColor, axis: int8) =
+  if fig.isNil:
+    return
   fig.inner.fill = linear(
     rgba(startColor.r, startColor.g, startColor.b, startColor.a),
     rgba(endColor.r, endColor.g, endColor.b, endColor.a),
-    axis = parseFillAxis(axis),
+    axis = axis.toBindingFillAxis().toFillGradientAxis(),
   )
 
 proc setFillLinear3Raw(
@@ -284,69 +357,70 @@ proc setFillLinear3Raw(
     axis: int8,
     midPos: uint8,
 ) =
+  if fig.isNil:
+    return
   fig.inner.fill = linear(
     rgba(sr, sg, sb, sa),
     rgba(mr, mg, mb, ma),
     rgba(er, eg, eb, ea),
-    axis = parseFillAxis(axis),
+    axis = axis.toBindingFillAxis().toFillGradientAxis(),
     midPos = midPos,
   )
 
 proc setFillLinear3(
-    fig: Fig,
-    startColor, midColor, endColor: RgbaColor,
-    axis: int8,
-    midPos: uint8,
+    fig: Fig, startColor, midColor, endColor: RgbaColor, axis: int8, midPos: uint8
 ) =
+  if fig.isNil:
+    return
   fig.inner.fill = linear(
     rgba(startColor.r, startColor.g, startColor.b, startColor.a),
     rgba(midColor.r, midColor.g, midColor.b, midColor.a),
     rgba(endColor.r, endColor.g, endColor.b, endColor.a),
-    axis = parseFillAxis(axis),
+    axis = axis.toBindingFillAxis().toFillGradientAxis(),
     midPos = midPos,
   )
 
 proc setRotation(fig: Fig, rotation: float32) =
+  if fig.isNil:
+    return
   fig.inner.rotation = rotation
 
 proc setCornersRaw(fig: Fig, topLeft, topRight, bottomLeft, bottomRight: float32) =
+  if fig.isNil:
+    return
   fig.inner.corners = [topLeft, topRight, bottomLeft, bottomRight]
 
 proc setCorners(fig: Fig, radii: CornerRadii) =
-  fig.inner.corners = [
-    radii.topLeft,
-    radii.topRight,
-    radii.bottomLeft,
-    radii.bottomRight,
-  ]
+  if fig.isNil:
+    return
+  fig.inner.corners =
+    [radii.topLeft, radii.topRight, radii.bottomLeft, radii.bottomRight]
 
 proc setStrokeRaw(fig: Fig, weight: float32, r, g, b, a: uint8) =
-  if fig.inner.kind != fdn.nkRectangle:
-    fig.inner = figWithKind(fig.inner, fdn.nkRectangle.int8)
-  fig.inner.stroke = RenderStroke(
-    weight: weight,
-    fill: fill(rgba(r, g, b, a)),
-  )
+  if fig.isNil:
+    return
+  fig.ensureRectangle()
+  fig.inner.stroke = RenderStroke(weight: weight, fill: fill(rgba(r, g, b, a)))
 
 proc setStrokeRaw(fig: Fig, weight: float32, color: RgbaColor) =
-  if fig.inner.kind != fdn.nkRectangle:
-    fig.inner = figWithKind(fig.inner, fdn.nkRectangle.int8)
-  fig.inner.stroke = RenderStroke(
-    weight: weight,
-    fill: fill(rgba(color.r, color.g, color.b, color.a)),
-  )
+  if fig.isNil:
+    return
+  fig.ensureRectangle()
+  fig.inner.stroke =
+    RenderStroke(weight: weight, fill: fill(rgba(color.r, color.g, color.b, color.a)))
 
 proc setStroke(fig: Fig, border: BorderSize, color: RgbaColor) =
-  if fig.inner.kind != fdn.nkRectangle:
-    fig.inner = figWithKind(fig.inner, fdn.nkRectangle.int8)
+  if fig.isNil:
+    return
+  fig.ensureRectangle()
   fig.inner.stroke = RenderStroke(
-    weight: border.width,
-    fill: fill(rgba(color.r, color.g, color.b, color.a)),
+    weight: border.width, fill: fill(rgba(color.r, color.g, color.b, color.a))
   )
 
 proc clearShadows(fig: Fig) =
-  if fig.inner.kind != fdn.nkRectangle:
-    fig.inner = figWithKind(fig.inner, fdn.nkRectangle.int8)
+  if fig.isNil:
+    return
+  fig.ensureRectangle()
   fig.inner.shadows = [RenderShadow(), RenderShadow(), RenderShadow(), RenderShadow()]
 
 proc setShadowRaw(
@@ -356,22 +430,14 @@ proc setShadowRaw(
     blur, spread, x, y: float32,
     r, g, b, a: uint8,
 ) =
-  if fig.inner.kind != fdn.nkRectangle:
-    fig.inner = figWithKind(fig.inner, fdn.nkRectangle.int8)
+  if fig.isNil:
+    return
+  fig.ensureRectangle()
   if shadowIndex < 0'i8 or shadowIndex >= ShadowCount.int8:
     return
 
-  var shadowStyle = ShadowStyle.NoShadow
-  case style
-  of 1'i8:
-    shadowStyle = ShadowStyle.DropShadow
-  of 2'i8:
-    shadowStyle = ShadowStyle.InnerShadow
-  else:
-    discard
-
   fig.inner.shadows[shadowIndex.int] = RenderShadow(
-    style: shadowStyle,
+    style: style.toBindingShadowStyle().toShadowStyle(),
     blur: blur,
     spread: spread,
     x: x,
@@ -386,22 +452,14 @@ proc setShadow(
     blur, spread, x, y: float32,
     color: RgbaColor,
 ) =
-  if fig.inner.kind != fdn.nkRectangle:
-    fig.inner = figWithKind(fig.inner, fdn.nkRectangle.int8)
+  if fig.isNil:
+    return
+  fig.ensureRectangle()
   if shadowIndex < 0'i8 or shadowIndex >= ShadowCount.int8:
     return
 
-  var shadowStyle = ShadowStyle.NoShadow
-  case style
-  of 1'i8:
-    shadowStyle = ShadowStyle.DropShadow
-  of 2'i8:
-    shadowStyle = ShadowStyle.InnerShadow
-  else:
-    discard
-
   fig.inner.shadows[shadowIndex.int] = RenderShadow(
-    style: shadowStyle,
+    style: style.toBindingShadowStyle().toShadowStyle(),
     blur: blur,
     spread: spread,
     x: x,
@@ -413,58 +471,86 @@ proc newRenderList(): RenderList =
   RenderList(inner: fdn.RenderList())
 
 proc copy(list: RenderList): RenderList =
+  if list.isNil:
+    return nil
   RenderList(inner: list.inner)
 
 proc clear(list: RenderList) =
+  if list.isNil:
+    return
   list.inner = fdn.RenderList()
 
 proc nodeCount(list: RenderList): int =
+  if list.isNil:
+    return 0
   list.inner.nodes.len
 
 proc rootCount(list: RenderList): int =
+  if list.isNil:
+    return 0
   list.inner.rootIds.len
 
 proc addRoot(list: RenderList, root: Fig): int16 =
+  if list.isNil or root.isNil:
+    return -1'i16
   list.inner.addRoot(root.inner).int16
 
 proc addChild(list: RenderList, parentIdx: int16, child: Fig): int16 =
+  if list.isNil or child.isNil:
+    return -1'i16
   try:
     list.inner.addChild(fdn.FigIdx(parentIdx), child.inner).int16
   except ValueError:
     -1'i16
 
 proc getNode(list: RenderList, nodeIdx: int16): Fig =
-  Fig(inner: list.inner.nodes[nodeIdx.int])
+  if list.isNil:
+    return nil
+  try:
+    Fig(inner: list.inner.nodes[nodeIdx.int])
+  except CatchableError:
+    nil
 
 proc getRootId(list: RenderList, rootIdx: int16): int16 =
-  list.inner.rootIds[rootIdx.int].int16
+  if list.isNil:
+    return -1'i16
+  try:
+    list.inner.rootIds[rootIdx.int].int16
+  except CatchableError:
+    -1'i16
 
 proc newRenders(): Renders =
   Renders(inner: fdn.Renders(layers: initOrderedTable[fdn.ZLevel, fdn.RenderList]()))
 
 proc clear(renders: Renders) =
+  if renders.isNil:
+    return
   renders.inner.layers.clear()
 
 proc containsLayer(renders: Renders, zLevel: int8): bool =
+  if renders.isNil:
+    return false
   renders.inner.contains(fdn.ZLevel(zLevel))
 
 proc addRoot(renders: Renders, zLevel: int8, root: Fig): int16 =
+  if renders.isNil or root.isNil:
+    return -1'i16
   try:
     renders.inner.addRoot(fdn.ZLevel(zLevel), root.inner).int16
   except CatchableError:
     -1'i16
 
 proc addChild(renders: Renders, zLevel: int8, parentIdx: int16, child: Fig): int16 =
+  if renders.isNil or child.isNil:
+    return -1'i16
   try:
-    renders.inner.addChild(
-      fdn.ZLevel(zLevel),
-      fdn.FigIdx(parentIdx),
-      child.inner,
-    ).int16
+    renders.inner.addChild(fdn.ZLevel(zLevel), fdn.FigIdx(parentIdx), child.inner).int16
   except CatchableError:
     -1'i16
 
 proc layerNodeCount(renders: Renders, zLevel: int8): int =
+  if renders.isNil:
+    return 0
   try:
     if not renders.containsLayer(zLevel):
       return 0
@@ -473,6 +559,8 @@ proc layerNodeCount(renders: Renders, zLevel: int8): int =
     0
 
 proc layerRootCount(renders: Renders, zLevel: int8): int =
+  if renders.isNil:
+    return 0
   try:
     if not renders.containsLayer(zLevel):
       return 0
@@ -481,10 +569,12 @@ proc layerRootCount(renders: Renders, zLevel: int8): int =
     0
 
 proc getLayerNode(renders: Renders, zLevel: int8, nodeIdx: int16): Fig =
+  if renders.isNil:
+    return nil
   try:
     Fig(inner: renders.inner[fdn.ZLevel(zLevel)].nodes[nodeIdx.int])
   except CatchableError:
-    newFig()
+    nil
 
 exportObject RgbaColor:
   constructor:
@@ -580,6 +670,6 @@ exportProcs:
 when ExportSiwinShim:
   include siwinshim_bindings
 
-writeFiles("bindings/generated", "FigDraw")
+writeFiles(GeneratedDir, "Figdraw")
 
 include generated/internal
