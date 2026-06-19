c7d8a9de3820c1910f0948da51a407ad693e9cee
xTrayambak <xtrayambak@gmail.com>
2026-06-19 18:05:49 +0530

    add: Nix shell + proper deps searching on linux

---
+++ shell.nix
@@ -0,0 +1,27 @@
+with import <nixpkgs> { };
+
+mkShell {
+  nativeBuildInputs = [
+    pkg-config
+    wayland
+    vulkan-loader
+
+    libX11
+    libxcb
+    libxcursor
+    libxkbcommon
+    libxrender
+    libGL
+  ];
+
+  LD_LIBRARY_PATH = lib.makeLibraryPath [
+    wayland.dev
+    vulkan-loader.dev
+    libX11.dev
+    libxcb.dev
+    libxcursor.dev
+    libxrender.dev
+    libxkbcommon.dev
+    libGL.dev
+  ];
+}
